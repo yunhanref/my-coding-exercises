190ae0d56ad3c775593ba237182a43486d536775
Mehmet Eren Gümüş <yunhanref@proton.me>
2026-09-08 12:36:44 +0300

    Create d2_multifile_example_fn.cpp

---
+++ cpp/d2_multifile_example_fn.cpp
@@ -0,0 +1,7 @@
+// We can use this file and its contents in the main.cpp file. Simply type the header of the function in main.cpp.
+// Note: Dont define/declare another function named "add" to prevent naming collision error.
+
+int add(int x, int y)
+{
+    return x+y;
+}
