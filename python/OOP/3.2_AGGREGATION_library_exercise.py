89422a83dc77ae555ec518890e84e5fb14358811
Mehmet Eren Gümüş <yunhanref@proton.me>
2026-09-08 12:27:06 +0300

    Update 3.2_AGGREGATION_library_exercise.py

--- python/OOP/3.2_AGGREGATION_library_exercise.py
+++ python/OOP/3.2_AGGREGATION_library_exercise.py
@@ -7,11 +7,9 @@ class Book:
 class Library:
     def __init__(self, name):
         self.name = name
-        self.books = []  # Kitapları tutacak liste
+        self.books = []
 
     def add_book(self, book):
-        # Aggregation burada gerçekleşiyor:
-        # Dışarıda oluşturulmuş nesne buraya aktarılıyor.
         self.books.append(book)
 
     def list_books(self):
@@ -20,21 +18,16 @@ class Library:
             print(f"- {book.title} ({book.author})")
 
 
-# 1. Bağımsız Kitap nesnelerini oluşturuyoruz
 b1 = Book("De Natura Deorum", "Cicero")
 b2 = Book("İçimdeki Karanlık", "Orhan Yenen")
 
-# 2. Kütüphane nesnesini oluşturuyoruz
 my_library = Library("Milli Kütüphane")
 
-# 3. Kitapları kütüphaneye ekliyoruz (Aggregation)
 my_library.add_book(b1)
 my_library.add_book(b2)
 
 my_library.list_books()
 
-# 4. Kütüphaneyi yok etsek bile kitaplar silinmez!
 del my_library
 
-# b1 nesnesi hala hafızada sapasağlam duruyor:
 print(f"\nKütüphane silindi ama kitap duruyor: {b1.title}, {b2.title}")
