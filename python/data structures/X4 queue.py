6d9431449fb9dce07d7cd828bb47b37277b89ec0
Mehmet Eren Gümüş <yunhanref@proton.me>
2026-09-21 15:42:39 +0300

    Update X4 queue.py

--- python/data structures/X4 queue.py
+++ python/data structures/X4 queue.py
@@ -1,5 +1,6 @@
 # SIMMPLER AND BETTER QUEUE UPDATE
-# Update: Added utility method that constantly asks the operations user may want to do instead of typing everythin statically in the source code.
+# Update: Added utility method that constantly asks the operations user may want to do... 
+# ...instead of typing everything statically in the source code which makes it uninteractive and useless in real life.
 #! queue
 class Queue():
     def __init__(self):
